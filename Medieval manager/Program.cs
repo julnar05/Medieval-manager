772569f7924ca6c7e9e2598b44421e1c8591d791
Jula <Jula@JULAHACK>
2022-01-28 18:27:22 +0100

    Dodaj pliki projektów.

---
+++ Medieval manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Medieval_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
